31ebfdb99b0f6a3962b01b56a2ff171dded91c11
romeroclaudino <romero_claudino@hotmail.com>
2017-03-20 04:40:33 -0300

    Atualizando informações do perfil

--- app/src/main/java/com/example/bernardojr/branchout/dados/UsuarioDAO.java
+++ app/src/main/java/com/example/bernardojr/branchout/dados/UsuarioDAO.java
@@ -30,16 +30,7 @@ public class UsuarioDAO {
     public static final String SUCESSO_CONSULTA_REALIZADA = "5";
     public static final String ERRO_CONSULTA_NAO_REALIZADA = "6";
 
-    public void validaLogin(String email, String senha) {
-        urlRequest = "http://obichoebom.azurewebsites.net/user/userlogin.php?email="+email+"&senha="+senha;
-        Log.v("EMAIL E SENHA", email+senha);
-        new BackgroundTask().execute("login", urlRequest);
-    }
-
     public void validaCadastro(String email, String senha, String nome, String dataNasc,String descricao, String meiosDecontato, String idiomas, String imagem) {
-        //        urlRequest = "http://obichoebom.azurewebsites.net/user/useradd.php?email="+email+"&senha="+
-//                senha+"&nome="+nome+"&datanasc="+dataNasc+"&descricao="+descricao+"&meiosdecontato="+
-//                meiosDecontato+"&idiomas="+idiomas+"&imagem="+imagem;
 
         urlRequest = "http://obichoebom.azurewebsites.net/user/useradd.php";
         HashMap<String, String> params = new HashMap<>();
@@ -53,8 +44,29 @@ public class UsuarioDAO {
         params.put("imagem", imagem);
 
         Log.v("URL COMPLETA", urlRequest);
-//        new BackgroundTask().execute("cadastro", urlRequest);
-        new BackgroundTaskPost(params).execute(urlRequest);
+        new BackgroundTaskPost(params).execute("cadastraU", urlRequest);
+    }
+
+    public void atualizaUsuario(String id, String senha, String nome, String dataNasc,String descricao, String meiosDecontato, String idiomas, String imagem) {
+        urlRequest = "http://obichoebom.azurewebsites.net/user/userupdate.php";
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("id", id);
+        params.put("senha", senha);
+        params.put("nome", nome);
+        params.put("datanasc", dataNasc);
+        params.put("descricao", descricao);
+        params.put("meiosdecontato", meiosDecontato);
+        params.put("idiomas", idiomas);
+        params.put("imagem", imagem);
+
+        new BackgroundTaskPost(params).execute("atualizaU", urlRequest);
+    }
+
+    public void validaLogin(String email, String senha) {
+        urlRequest = "http://obichoebom.azurewebsites.net/user/userlogin.php?email="+email+"&senha="+senha;
+        Log.v("EMAIL E SENHA", email+senha);
+        new BackgroundTask().execute("login", urlRequest);
     }
 
     public void enviaLocalizacao(String id, String x, String y) {
@@ -78,21 +90,12 @@ public class UsuarioDAO {
         new BackgroundTask().execute("match", urlRequest);
     }
 
-    public void atualizaUsuario(String id, String senha, String nome, String dataNasc,String descricao, String meiosDecontato, String idiomas, String imagem) {
-        urlRequest = "http://obichoebom.azurewebsites.net/user/userupdate.php?id="+id+"&senha="+
-                senha+"&nome="+nome+"&datanasc="+dataNasc+"&descricao="+descricao+"&meiosdecontato="+
-                meiosDecontato+"&idiomas="+idiomas+"&imagem="+imagem;
-
-        new BackgroundTask().execute("atualizaU", urlRequest);
-    }
-
     private class BackgroundTask extends AsyncTask<String, Void, String[]> {
 
         @Override
         protected String[] doInBackground(String... urls) {
             String[] response = new String[2];
             response[0] = urls[0];
-//            response[1] = Funcoes.getStringResponse(urls[1]);
             response[1] = Funcoes.getStringResponse(urls[1], null);
             return response;
         }
@@ -102,14 +105,10 @@ public class UsuarioDAO {
             Log.v("RESPOSTA DO SERVIDOR", response[1]);
             if(response[0].equals("login"))
                 LoginActivity.mostraMensagem(response[1]);
-            else if (response[0].equals("cadastro"))
-                CadastroActivity.mostraMensagem(response[1]);
             else if (response[0].equals("localizacao"))
                 return;
             else if (response[0].equals("match"))
                 return;
-            else if(response[0].equals("atualizaU"))
-                return;  //Caso queira pode chamar um método utilizando a resposta como parâmetro
         }
     }
 
@@ -149,7 +148,7 @@ public class UsuarioDAO {
         }
     }
 
-    private class BackgroundTaskPost extends AsyncTask<String, Void, String> {
+    private class BackgroundTaskPost extends AsyncTask<String, Void, String[]> {
         private HashMap<String, String> params;
 
         public BackgroundTaskPost(HashMap<String, String> params) {
@@ -157,13 +156,21 @@ public class UsuarioDAO {
         }
 
         @Override
-        protected String doInBackground(String... urls) {
-            return  Funcoes.getStringResponse(urls[0], this.params);
+        protected String[] doInBackground(String... urls) {
+            String[] response = new String[2];
+            response[0] = urls[0];
+            response[1] = Funcoes.getStringResponse(urls[1], this.params);
+
+            return response;
         }
 
         @Override
-        protected void onPostExecute(String response) {
-            CadastroActivity.mostraMensagem(response);
+        protected void onPostExecute(String[] response)
+        {
+            if(response[0].equals("cadastraU"))
+                CadastroActivity.mostraMensagem(response[1]);
+            else if (response[0].equals("atualizaU"))
+                return;
         }
     }
 }
